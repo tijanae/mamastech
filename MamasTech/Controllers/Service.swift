8ac44bc0d15db55e62abea544628b3f8ca5a57ec
Tia Lendor <TiaLendon@pursuit.org>
2019-12-17 23:13:33 -0500

    upload

---
+++ MamasTech/Controllers/Service.swift
@@ -0,0 +1,24 @@
+//
+//  Service.swift
+//  MamasTech
+//
+//  Created by Tia Lendor on 11/1/19.
+//  Copyright © 2019 Tia Lendor. All rights reserved.
+//
+
+import UIKit
+
+class Service: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
